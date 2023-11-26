0fa0596cb94e880f7028a02c1e8f41786a08a2a9
solutions-plumbing <Aroniascoder@gmail.com>
2023-11-26 08:37:02 -0800

    updates

--- app/(dashboard)/(routes)/resume-generator/page.tsx
+++ app/(dashboard)/(routes)/resume-generator/page.tsx
@@ -638,7 +638,7 @@ ${stringifiedMappedFormValues}
       });
 
    
-       return; 
+       return false; 
     // make API call 
     try {
       const userMessage: ChatCompletionRequestMessage = { role: "user", content: promptString };
@@ -2307,7 +2307,7 @@ ${stringifiedMappedFormValues}
                 Generate Resume
               </Button>
 
-               <Button
+   {/*            <Button
                 onClick={generateCoverLetter}
                 className="col-span-12 lg:col-span-2 w-full"
                 // type="submit" 
@@ -2316,7 +2316,7 @@ ${stringifiedMappedFormValues}
                 size="icon"
               >
                 Generate Cover Letter
-              </Button>   
+              </Button>   */}
 
               {/* <Button
                 onClick={generateDocx}
