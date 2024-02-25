aa15fa3bfa7dec3fbf02fac0dbdb54f9b2dee267
solutions-plumbing <Aroniascoder@gmail.com>
2024-02-25 13:57:46 -0800

    updates

--- app/api/resume-generator/route.ts
+++ app/api/resume-generator/route.ts
@@ -41,8 +41,8 @@ export async function POST(
     // }
 
     const response = await openai.createChatCompletion({
-      // model: "gpt-3.5-turbo",
-      model: "gpt-4-0125-preview",
+      model: "gpt-3.5-turbo",
+      // model: "gpt-4-0125-preview",
       messages
     });
 
