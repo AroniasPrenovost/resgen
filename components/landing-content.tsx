65ae9342e55c4eb2a19099fdd0d9d66e33b32ab3
solutions-plumbing <Aroniascoder@gmail.com>
2024-06-01 06:32:54 -0700

    updates

--- components/landing-content.tsx
+++ components/landing-content.tsx
@@ -36,7 +36,7 @@ const testimonials = [
 export const LandingContent = () => {
   return (
     <div className="px-10 pb-20">
-      <h2 className="text-center text-4xl text-white font-extrabold mb-10">What users are saying...</h2>
+      <h2 className="text-center text-4xl text-white font-extrabold mb-10">See what users are saying...</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {testimonials.map((item) => (
           <Card key={item.description} className="bg-[#192339] border-none text-white">
