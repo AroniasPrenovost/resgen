d853d53f6feff5474a9428bd3c1d547874d7655d
solutions-plumbing <Aroniascoder@gmail.com>
2024-06-01 07:04:51 -0700

    updates

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -80,8 +80,10 @@ export const Sidebar = ({
                 <li><b>4.</b> <em>Optional:</em> If you do not own Microsoft Word, take the downloaded file and upload it to <Link className={cn(
                   "hover:text-white transition",
                    "text-zinc-400",
-                )} target="_blank" href='https://docs.google.com/document/u/0/'>Google Docs</Link> to modify formatting.
+                )} target="_blank" href='https://drive.google.com/drive/home'>Google Drive</Link> to modify formatting:
                 </li>
+                <span style={{marginRight: '14px'}}></span>- Click '+ New' <br />
+                <span style={{marginRight: '14px'}}></span>- Click 'File upload' 
               </ol>
           </div>
         </div>
