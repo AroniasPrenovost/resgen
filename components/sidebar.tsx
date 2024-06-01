31fa835841c2daef50b508b896496ba94eeea087
solutions-plumbing <Aroniascoder@gmail.com>
2024-06-01 07:15:14 -0700

    updates

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -86,10 +86,10 @@ export const Sidebar = ({
           </div>
         </div>
       </div>
-      <FreeCounter
+{/*      <FreeCounter
         apiLimitCount={apiLimitCount}
         isPro={isPro}
-      />
+      />*/}
     </div>
   );
 };
