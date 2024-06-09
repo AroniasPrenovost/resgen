d5c2bb6ea81bc0f994be12ba368a2a11646ab581
solutions-plumbing <Aroniascoder@gmail.com>
2024-06-09 06:21:11 -0700

    table test

--- lib/resume-generator.ts
+++ lib/resume-generator.ts
@@ -8,8 +8,11 @@ import {
   TabStopType,
   TextRun,
   ExternalHyperlink,
+  Table,
   TableRow,
   TableCell,
+  VerticalAlign,
+  TextDirection,
 } from "docx";
 
 type PersonalInfo = {
@@ -55,24 +58,7 @@ export class DocumentCreator {
 
 
 
-            // test
-            //
-            //
 
-           new TableRow({
-                children: [
-                    new TableCell({
-                        children: [new Paragraph("hello")],
-                    }),
-                    new TableCell({
-                        children: [new Paragraph("hello")],
-                    }),
-                ],
-            }),
-
-           //
-           //
-           // test
 
             /*
 
@@ -273,6 +259,94 @@ export class DocumentCreator {
             //     "This CV was generated in real-time based on my Linked-In profile from my personal website www.dolan.bio.",
             //   alignment: AlignmentType.CENTER
             // })
+
+
+
+
+
+            // test
+            //
+            //
+ 
+
+// const doc = new Document({
+//     sections: [
+//         {
+//             children: [
+                new Table({
+                    rows: [
+                        new TableRow({
+                            children: [
+                                new TableCell({
+                                    children: [new Paragraph({}), new Paragraph({})],
+                                    verticalAlign: VerticalAlign.CENTER,
+                                }),
+                                new TableCell({
+                                    children: [new Paragraph({}), new Paragraph({})],
+                                    verticalAlign: VerticalAlign.CENTER,
+                                }),
+                                new TableCell({
+                                    children: [new Paragraph({ text: "bottom to top" }), new Paragraph({})],
+                                    textDirection: TextDirection.BOTTOM_TO_TOP_LEFT_TO_RIGHT,
+                                }),
+                                new TableCell({
+                                    children: [new Paragraph({ text: "top to bottom" }), new Paragraph({})],
+                                    textDirection: TextDirection.TOP_TO_BOTTOM_RIGHT_TO_LEFT,
+                                }),
+                            ],
+                        }),
+                        new TableRow({
+                            children: [
+                                new TableCell({
+                                    children: [
+                                        new Paragraph({
+                                            text: "Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah Blah",
+                                            heading: HeadingLevel.HEADING_1,
+                                        }),
+                                    ],
+                                }),
+                                new TableCell({
+                                    children: [
+                                        new Paragraph({
+                                            text: "This text should be in the middle of the cell",
+                                        }),
+                                    ],
+                                    verticalAlign: VerticalAlign.CENTER,
+                                }),
+                                new TableCell({
+                                    children: [
+                                        new Paragraph({
+                                            text: "Text above should be vertical from bottom to top",
+                                        }),
+                                    ],
+                                    verticalAlign: VerticalAlign.CENTER,
+                                }),
+                                new TableCell({
+                                    children: [
+                                        new Paragraph({
+                                            text: "Text above should be vertical from top to bottom",
+                                        }),
+                                    ],
+                                    verticalAlign: VerticalAlign.CENTER,
+                                }),
+                            ],
+                        }),
+                    ],
+                }),
+            // ],
+        // },
+    // ],
+// });
+
+
+
+           //
+           //
+           // test
+
+
+
+
           ]
         }
       ]
