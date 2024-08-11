58795fd2d9a071ef319e7d44cc7612545e1b9891
solutions-plumbing <Aroniascoder@gmail.com>
2024-08-11 07:49:44 -0700

    updating content and removing price reduction

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -73,10 +73,9 @@ export const Sidebar = ({
               <ol>
                 <li><b>1. </b>Enter your work experience and other relevant information.</li>
                 <br/>
-                <li><b>2. </b>A single payment of 
+                <li><b>2. </b>A single payment of <b style={{color:"orange"}}>$4.99</b> gets you 30 days of unlimited access.</li>
                 {/*<s><b>$9.99</b></s> */}
-                <b style={{color:"orange"}}>$4.99</b> 
-                gets you 30 days of unlimited access.</li>
+            
                 <br/>
                 <li><b>3. </b>Our AI assistant generates your new resume.</li>
                 <br/>
