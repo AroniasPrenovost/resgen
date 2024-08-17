6facdf5102e69c335f4b748be9b29bd3f8f5f0de
solutions-plumbing <Aroniascoder@gmail.com>
2024-08-17 09:09:02 -0700

    content update

--- app/(dashboard)/(routes)/resume-generator/page.tsx
+++ app/(dashboard)/(routes)/resume-generator/page.tsx
@@ -56,7 +56,7 @@ const ResumeGeneratorPage = () => {
   //
   //
 
-  const [subheadline, setSubheadline] = useState('Instant professional resume creation. Get 30 days of access for ');
+  const [subheadline, setSubheadline] = useState('Instant professional resume generation. Get 30 days of access for ');
   // const defaultPrice = '$9.99';
   // const salePrice = '$3.99';
 
