8a13db1604dfb69f00f0cbc08e2e416e7c870b22
aron_prenovost <arons_stuff@aron-prenovosts-Mac-mini.local>
2024-09-14 08:38:32 -0700

    adding disclaimer at bottom of form

--- components/ui/empty.tsx
+++ components/ui/empty.tsx
@@ -2,12 +2,15 @@ import Image from "next/image";
 
 
 interface EmptyProps {
-  label: string;
+  label?: string;
 }
 
 export const Empty = ({
   label
 }: EmptyProps) => {
+
+  const hasLabel = label.length;
+
   return (
     <div className="h-full p-4 flex flex-col items-center justify-center">
       <div className="relative h-72 w-72">
@@ -19,8 +22,13 @@ export const Empty = ({
             <Image fill alt="Logo" src="/transcript.png" />
           </div>*/}
       <p className="text-muted-foreground text-sm text-center">
-        {label}<br/>
+      {hasLabel && (
+        {label}
+      )} else {
+        <strong>NOTICE:</strong> We do not store any of your personal or financial data.<br/>
         All information is saved in your local browser session only.
+      }
+
       </p>
     </div>
   );
