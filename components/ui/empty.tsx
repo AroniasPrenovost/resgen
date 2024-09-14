980ecb10f7dfb3e561ce3a14631e6d1f1021d253
aron_prenovost <arons_stuff@aron-prenovosts-Mac-mini.local>
2024-09-14 08:47:25 -0700

    adding disclaimer at bottom of form

--- components/ui/empty.tsx
+++ components/ui/empty.tsx
@@ -1,36 +1,29 @@
 import Image from "next/image";
 
-
 interface EmptyProps {
   label?: string;
 }
 
-export const Empty = ({
-  label
-}: EmptyProps) => {
-
-  const hasLabel = label.length;
+export const Empty = ({ label }: EmptyProps) => {
+  const hasLabel = !!label;
 
   return (
     <div className="h-full p-4 flex flex-col items-center justify-center">
       <div className="relative h-72 w-72">
-        {/*<Image src="/empty.png" fill alt="Empty" />*/}
-            <Image fill alt="Logo" src="/transcript.png" />
+        {/* <Image src="/empty.png" fill alt="Empty" /> */}
+        <Image fill alt="Logo" src="/transcript.png" />
       </div>
 
-   {/*             <div className="relative h-8 w-8 mr-4">
-            <Image fill alt="Logo" src="/transcript.png" />
-          </div>*/}
       <p className="text-muted-foreground text-sm text-center">
-      {hasLabel &&
-        {label}
-      }
-
-      {!hasLabel 
-        <strong>NOTICE:</strong> We do not store any of your personal or financial data.<br/>
-        All information is saved in your local browser session only.
-      }
-
+        {hasLabel ? (
+          label
+        ) : (
+          <>
+            <b>NOTICE:</b> We do not store any of your personal or financial data.
+            <br />
+            All information is saved in your local browser session only.
+          </>
+        )}
       </p>
     </div>
   );
