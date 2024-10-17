d2d7c11c234410d8e54e9b4a34337970de7e98d8
aron_prenovost <arons_stuff@aron-prenovosts-Mac-mini.local>
2024-10-17 07:42:11 -0700

    updating stripe payment processing line

--- components/ui/empty.tsx
+++ components/ui/empty.tsx
@@ -10,7 +10,6 @@ export const Empty = ({ label }: EmptyProps) => {
   return (
     <div className="h-full p-4 flex flex-col items-center justify-center">
       <div className="relative h-72 w-72">
-        {/* <Image src="/empty.png" fill alt="Empty" /> */}
         <Image fill alt="Logo" src="/transcript.png" />
       </div>
 
@@ -20,8 +19,22 @@ export const Empty = ({ label }: EmptyProps) => {
         ) : (
           <>
             <b>NOTICE</b>
-            <br/>
+            <br />
             We do not collect or sell your personal data.
+            <br />
+            <span className="flex items-center justify-center">
+              Payments are processed with
+              <Image
+                width={30}
+                height={30}
+                alt="Stripe logo"
+                src="/p.png"
+                className="inline-block ml-1"
+                style={{
+                  marginTop: "2px",
+                }}
+              />.
+            </span>
           </>
         )}
       </p>
