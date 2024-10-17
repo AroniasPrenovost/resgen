cfaf007afc36ea0cd68bd6efa46a4f237e573b1e
aron_prenovost <arons_stuff@aron-prenovosts-Mac-mini.local>
2024-10-17 11:45:00 -0700

    fixing type issue

--- app/(dashboard)/(routes)/resume-generator/page.tsx
+++ app/(dashboard)/(routes)/resume-generator/page.tsx
@@ -3217,7 +3217,7 @@ ${stringifiedMappedFormValues}
                   onMouseEnter={() => setFormSubmitButtonIsHovered(true)}
                   onMouseLeave={() => setFormSubmitButtonIsHovered(false)}
                   >
-                   Generate New Resume
+                   Generate and Download New Resume
                 {/*
 
                   <Image width={20} height={20} alt="Stripe logo" src="/stripe.png" />
