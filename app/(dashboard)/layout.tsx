665e551b3332f3b0737dba769e7864b616fbf4fe
aron_prenovost <arons_stuff@aron-prenovosts-Mac-mini.local>
2024-10-19 06:02:10 -0700

    updating loading prefix

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -11,18 +11,18 @@ const DashboardLayout = async ({
   // const apiLimitCount = await getApiLimitCount();
   // const isPro = await checkSubscription();
 
-  return ( 
+  return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
         {/*<Sidebar isPro={isPro} apiLimitCount={apiLimitCount} />*/}
        <Sidebar isPro={true} apiLimitCount={0} />
       </div>
       <main className="md:pl-72 pb-10">
-        <Navbar /> 
+        <Navbar />
         {children}
       </main>
     </div>
    );
 }
- 
+
 export default DashboardLayout;
