99c24d1fad411de26836ed099e53c28e9f60adfd
aron_prenovost <arons_stuff@aron-prenovosts-Mac-mini.local>
2024-10-29 13:31:58 -0700

    adding textarea support

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils"
 export interface TextareaProps
   extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
-const Textarea = React.forwardRef<HTMLInputElement, TextareaProps>(
+const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({
     className,
     // type,
