4df89351fa2d0e18b67a94eb8a64ab26390632d4
aron_prenovost <arons_stuff@aron-prenovosts-Mac-mini.local>
2025-04-06 07:15:52 -0700

    updates

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -88,11 +88,11 @@ export const Sidebar = ({
               <ol>
          {/*       <li><b>1. </b><b style={{color:"orange"}}>$3.99</b> gets you access for 30 days.</li>
                   <br/>*/}
-                <li><b style={{color: 'orange', fontSize: '14px'}}>1. </b>Copy + paste a job description you&apos;d like the resume tailored to (optional).</li>
+                <li><b style={{color: 'orange', fontSize: '14px'}}>1. </b>Add a job description to tailor your resume to a specific role (optional).</li>
                 <br/>
-                <li><b style={{color: 'orange', fontSize: '14px'}}>2. </b>Upload your current resume and watch as ResumAI analyzes and rewrites the content.</li>
+                <li><b style={{color: 'orange', fontSize: '14px'}}>2. </b>Upload your current resume and watch as ResumAI analyzes and improves the content.</li>
                 <br/>
-                <li><b style={{color: 'orange', fontSize: '14px'}}>3. </b>When you generate the new resume template, the AI assistant performs a final review and generates your file.</li>
+                <li><b style={{color: 'orange', fontSize: '14px'}}>3. </b>When you generate the new resume template, the AI assistant performs a final revision process and generates your file.</li>
                 <br/>
 
                 <li>A one-time payment of <b style={{ color: "orange"}}>$9.99</b> gets you <b>30</b> days of access:<br/>
