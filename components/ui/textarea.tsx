4b564b8457298afd0002a2cbe885ede9ba4f9702
aron_prenovost <arons_stuff@aron-prenovosts-Mac-mini.local>
2025-09-01 11:57:46 -0700

    Updating form inputs

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -13,11 +13,12 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextAreaProps>(
   }, ref) => {
     return (
       <textarea
-        rows={4}
+        maxRows={4}
         cols={50}
         // type={type}
         style={{
           lineHeight: "unset",
+          height: "auto",
         }}
         className={cn(
           "flex h-9 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
